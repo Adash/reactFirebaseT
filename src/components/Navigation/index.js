52d51f3376d49ea804d5f6addbb62cd0994aadc7
Adam <Adam@isoxford.local>
2019-04-24 13:07:39 +0100

    routing

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => (
   <div>
-    <h1>App</h1>
+    <ul>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>SIGN_IN</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.LANDING}>LANDING</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.HOME}>HOME</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ACCOUNT}>ACCOUNT</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ADMIN}>ADMIN</Link>
+      </li>
+    </ul>
   </div>
 );
 
