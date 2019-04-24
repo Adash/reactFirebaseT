aae994f16e776abf1eda1f65df1e15a827e077f6
Adam <Adam@isoxford.local>
2019-04-24 14:39:32 +0100

    firebase auth API setup

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,29 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import config from './config';
+
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+  // *** Auth API ***
+
+  fbCreateUser = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  fbSignIn = (email, password) => {
+    this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  fbSignOut = () => this.auth.signOut();
+
+  fbPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  fbPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
