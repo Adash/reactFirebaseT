e47026d2d74fad651c54a58e7d40817d97363a8b
Adash <adash108@yahoo.com>
2019-05-12 17:15:28 +0100

    added user details information and moved user fetfch logic from admin component to userlist

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import { withAuthorisation } from '../Session';
 import * as ROLES from '../../constants/roles';
+import * as ROUTES from '../../constants/routes';
 
-class AdminPage extends Component {
+
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+    
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN} component={UserList} />
+    </Switch>
+
+    <h4>Restricted area.</h4>
+    <p>Page accessible only from authorised Admin users</p>
+  </div>
+);
+
+class UserItemBase extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      loading: false,
+      user:null,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase
+      .user(this.props.match.params.id)
+      .on('value', snapshot => {
+        this.setState({
+          user: snapshot.val(),
+          loading: false,
+        });
+        console.log(snapshot.val())
+      });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.user(this.props.match.params.id).off();
+    
+  }
+
+  render() {
+    const { user, loading } = this.state;
+
+    return (
+      <div>
+        <h3>{ loading ? 'Loading...' : 'User' }</h3>
+        { user && 
+          <div>
+            <p>Name: { user.username }</p>
+            <p>Email: { user.email } </p>
+            <p>User type: { user.roles[ROLES.ADMIN] ? 'Admin User' : 'User' } </p>
+            <p>ID: ({ this.props.match.params.id })</p>
+          </div>
+        }
+      </div>
+    )
+  }
+
+}
+
+class UserListBase extends Component {
   constructor(props){
     super(props)
 
@@ -15,7 +81,7 @@ class AdminPage extends Component {
     }
   }
 
-componentDidMount() {
+  componentDidMount() {
     this.setState({ loading: true });
 
     this.props.firebase.users().on('value', snapshot => {
@@ -36,46 +102,43 @@ componentDidMount() {
   componentWillUnmount() {
     this.props.firebase.users().off();
   }
-  
-  
+
+
   render() {
     const { users, loading } = this.state;
-
     return (
       <div>
-        <h1>Admin</h1>
-
+        <h3>Users</h3>
         { loading && <div>Loading...</div> }
-        
-        <UsersList users={users} />
-
-        <h4>Restricted area.</h4>
-        <p>Page accessible only from authorised Admin users</p>
+        <ul>
+          { users.map(user => (
+            <li key={user.uid}>
+              <span>
+                <strong>ID:</strong> { user.uid }
+              </span>
+              <span>
+                <strong>E-Mail:</strong> { user.email }
+              </span>
+              <span>
+                <strong>Username:</strong> { user.username }
+              </span>
+              <span>
+                <Link to={`${ROUTES.ADMIN}/${user.uid}`}>
+                  Details
+                </Link>
+              </span>
+            </li>
+          )) }
+        </ul>
       </div>
     );
   }
 }
-
-const UsersList = ({users}) => (
-  <ul>
-    { users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID:</strong> { user.uid }
-        </span>
-        <span>
-          <strong>E-Mail:</strong> { user.email }
-        </span>
-        <span>
-          <strong>Username:</strong> { user.username }
-        </span>
-      </li>
-    )) }
-  </ul>
-);
-
 const condition = authUser => 
   authUser && !!authUser.roles[ROLES.ADMIN];
+
+const UserList = withFirebase(UserListBase);
+const UserItem = withFirebase(UserItemBase);
 
 export default compose(
   withAuthorisation(condition),
